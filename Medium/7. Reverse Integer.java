887894bd05db929e211b59d7649176b89553387d
Ajay Kumar S B <111496801+Tazified@users.noreply.github.com>
2024-04-21 23:14:22 +0530

    Create 7. Reverse Integer.java
    Code
    Testcase
    Testcase
    Test Result
    7. Reverse Integer
    Solved
    Medium
    Topics
    Companies
    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
    
    Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
    
     
    
    Example 1:
    
    Input: x = 123
    Output: 321
    Example 2:
    
    Input: x = -123
    Output: -321
    Example 3:
    
    Input: x = 120
    Output: 21
     
    
    Constraints:
    
    -231 <= x <= 231 - 1

---
+++ Medium/7. Reverse Integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int num = x;
+        long reverse = 0;
+        if (num > Integer.MAX_VALUE || num < Integer.MIN_VALUE) {
+            return 0;
+        }
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+        if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) reverse;
+    }
+
+}
